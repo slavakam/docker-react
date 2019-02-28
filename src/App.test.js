8d4c1611e698d012f35b1c5739f4f4e19160d78f
Vyacheslav Kamenev <vy.kamenev@gmail.com>
2019-02-28 18:32:50 +0200

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('test 1', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('test 2', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
